2b87099ced118302b94fd223a6d997d5a6ef6911
Donhuey <35469316+donhuey@users.noreply.github.com>
2023-01-25 15:43:12 -0600

    Update main.js

--- main.js
+++ main.js
@@ -34,8 +34,10 @@ scene.background = new THREE.Color ("rgba(153,153,151,255)");
             // let loader = THREE.GLTFLoader;
             loader.setDRACOLoader(dracoLoader);
             loader.load("/HustleBasicPro.gltf", function (gltf) {
-                gltf.scene.scale.set(0.04, 0.04, 0.04); 
-                gltf.scene.position.y = -1.5;
+                gltf.scene.scale.set(0.03, 0.03, 0.03); 
+                gltf.scene.position.y = -.5;
+                gltf.scene.position.x = 1;
+                // gltf.scene.position.z = -4;
                 let deck = gltf.scene;
                 scene.add(deck);
     
@@ -80,7 +82,7 @@ const sizes = {
 
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height,0.1,1000)
-camera.position.set(1.8,3,3);
+camera.position.set(2.9,3,3);
 // camera.position.x = 0;
 // camera.position.y = 1;
 // camera.position.z = 0;
